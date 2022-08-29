e6b371d4caa23bc39e917d749de55f3712d20cee
xiaozhuoge <270775449@qq.com>
2022-08-29 11:04:11 +0800

    first

--- commomvo/ResultData.go
+++ commomvo/ResultData.go
@@ -6,3 +6,8 @@ type ResultData[T any, V any] struct {
 	ExtData V      `json:"ExtData"`
 	Data    T      `json:"Data"`
 }
+
+type PageBean[T any] struct {
+	TotalCount  int64 `json:"TotalCount"`
+	CurrentData []T   `json:"CurrentData"`
+}
