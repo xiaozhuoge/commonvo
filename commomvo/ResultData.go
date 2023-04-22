c099ca8bdf3d6d73f9cfc0f4a6d4d982068dc6d3
xiaozhuoge <270775449@qq.com>
2023-04-22 09:09:47 +0800

    update

--- commomvo/ResultData.go
+++ commomvo/ResultData.go
@@ -7,6 +7,50 @@ type ResultData[T any, V any] struct {
 	Data    T      `json:"Data"`
 }
 
+type ResultDataWithoutExtParameters[T any] struct {
+	Code    int32  `json:"Code"`
+	Message string `json:"Message"`
+	Data    T      `json:"Data"`
+}
+
+type ResultDataWithTwoExtParameters[T any, V any, B any] struct {
+	Code       int32  `json:"Code"`
+	Message    string `json:"Message"`
+	ExtData    V      `json:"ExtData"`
+	ExtDataTwo B      `json:"ExtDataTwo"`
+	Data       T      `json:"Data"`
+}
+
+type ResultDataWithThreeExtParameters[T any, V any, B any, C any] struct {
+	Code         int32  `json:"Code"`
+	Message      string `json:"Message"`
+	ExtData      V      `json:"ExtData"`
+	ExtDataTwo   B      `json:"ExtDataTwo"`
+	ExtDataThree C      `json:"ExtDataThree"`
+	Data         T      `json:"Data"`
+}
+
+type ResultDataWithFourExtParameters[T any, V any, B any, C any, D any] struct {
+	Code         int32  `json:"Code"`
+	Message      string `json:"Message"`
+	ExtData      V      `json:"ExtData"`
+	ExtDataTwo   B      `json:"ExtDataTwo"`
+	ExtDataThree C      `json:"ExtDataThree"`
+	ExtDataFour  D      `json:"ExtDataFour"`
+	Data         T      `json:"Data"`
+}
+
+type ResultDataWithFiveExtParameters[T any, V any, B any, C any, D any, E any] struct {
+	Code         int32  `json:"Code"`
+	Message      string `json:"Message"`
+	ExtData      V      `json:"ExtData"`
+	ExtDataTwo   B      `json:"ExtDataTwo"`
+	ExtDataThree C      `json:"ExtDataThree"`
+	ExtDataFour  D      `json:"ExtDataFour"`
+	ExtDataFive  E      `json:"ExtDataFive"`
+	Data         T      `json:"Data"`
+}
+
 type PageBean[T any] struct {
 	TotalCount  int64 `json:"TotalCount"`
 	CurrentData []T   `json:"CurrentData"`
